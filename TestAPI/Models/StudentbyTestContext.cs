09fdf51df0ffa882ef389b41516a28b46526e9dc
personal <hroudaadam@gmail.com>
2020-06-24 11:57:38 +0200

    init

---
+++ TestAPI/Models/StudentbyTestContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestAPI.Models
+{
+    public class StudentbyTestContext: DbContext
+    {
+        public StudentbyTestContext(DbContextOptions<StudentbyTestContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ TestAPI/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestAPI.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+
+        // role
+    }
+}
