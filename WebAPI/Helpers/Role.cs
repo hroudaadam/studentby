c85bf8197ef1e52441a9b00c3fe536b2fef461f3
personal <hroudaadam@gmail.com>
2020-08-30 09:05:58 +0200

    vytvoreni pomocnych trid

---
+++ WebAPI/Helpers/Role.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestAPI.Helpers
+{
+    public static class Role
+    {
+        public const string Customer = "Customer";
+        public const string Student = "Student";
+        public const string Operator = "Operator";
+    }
+}
---
+++ WebAPI/Helpers/StudentbyException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestAPI.Helpers
+{
+    public class StudentbyException : Exception
+    {
+        public StudentbyException(string message) : base(message)
+        {
+        }
+    }
+}
